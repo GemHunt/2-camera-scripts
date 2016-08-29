f064a864500bb1fd4425fd5a720bedf44de2b160
Paul Krush <pkrush@GemHunt.com>
2016-08-29 12:58:58 -0500

    crop script works

--- crop.py
+++ crop.py
@@ -10,21 +10,24 @@ conn = sqlite3.connect('coins.db')
 sys.path.append('/home/pkrush/caffe/python')
 
 
-def crop(num,x,y,radius):
+def crop(num, x, y, radius):
     # type: (object, object, object, object) -> object
     image_name = 'raw/' + str(num) + '.png'
-    img = cv2.imread(image_name, 0)
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    crop_name = 'crops/' + str(num) + '.png'
+
+    img = cv2.imread(image_name)
     # The coin sometimes is off the top of the image. In this case this math will cause the coin to crop off center.
     x = x + 2
     top_y = y - radius
     if top_y < 0:
         top_y = 0
     bottom_y = top_y + radius + radius
-    dst = cimg[top_y:bottom_y, x - radius:x + radius]
-    cv2.imshow('crop', dst)
-    cv2.waitKey(0)
-    return;
+    dst = img[top_y:bottom_y, x - radius:x + radius]
+    cv2.imwrite(crop_name, dst)
+    #cv2.imshow('crop', dst)
+    #cv2.waitKey(0)
+    return
+
 
 c = conn.cursor()
 c.execute("""Select
@@ -41,14 +44,14 @@ and cc2.imageID % 2 = 1
 and ((cc1.xMatch - cc2.x) * (cc1.xMatch - cc2.x)) + ((cc1.yMatch - cc2.y) * (cc1.yMatch - cc2.y)) < 200
 and cc1.x > 390
 and cc1.x <  840
-and cc1.imageID > 30999
-and cc1.imageID < 32000
+--and cc1.imageID > 30999
+--and cc1.imageID < 32000
 and cc1.imageID not in (select imageID from badImages)
 order by 1""")
 all_rows = c.fetchall()
 for row in all_rows:
     print('{0} : {1}, {2}'.format(row[0], row[1], row[2]))
     crop(row[0], row[1], row[2], 351)
-    crop(row[0]+1, row[3], row[4], 327)
+    crop(row[0] + 1, row[3], row[4], 327)
 
 cv2.destroyAllWindows()
