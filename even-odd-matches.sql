e810bb8297db10d6b689287a1da1367f8d773c30
Paul Krush <pkrush@GemHunt.com>
2016-09-02 17:45:01 -0500

    Many image labeling progress

--- even-odd-matches.sql
+++ even-odd-matches.sql
@@ -1,6 +1,9 @@
-Select * 
-from images
-where bad_image = 0 
-and angle is not null 
+Select *
+from images i1
+join images i2
+on i1.imageID % 2 = 0
+and i2.imageID % 2 = 1
+and i1.imageID = i2.ImageID - 1
+and not (i1.bad_image = 1 or i2.bad_image = 1)
+and i1.heads = i2.heads
 Order by 1
- 
--- label-many-crops.py
+++ label-many-crops.py
@@ -10,18 +10,12 @@ def open_windows():
 
     for x in range(0, 4):
         for y in range(0, 4):
-            start = time.time()
             if len(imageIDs) == open_windows.count:
                 continue
 
             imageID = imageIDs[open_windows.count]
-#            print(1 + time.time() - start)
             open_windows.count = open_windows.count + 1
-            print open_windows.count
-            if open_windows.count == 61:
-                print 'dd'
-
-            angle = imageID[1]
+            #angle = int(imageID[1])
             imageID = str(imageID[0])
             filename = '/home/pkrush/2-camera-scripts/crops/' + imageID + '.png'
             crop = cv2.imread(filename)
@@ -29,6 +23,7 @@ def open_windows():
                 continue
             crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
             crop = cv2.resize(crop, (thumb_nail_size,thumb_nail_size), interpolation=cv2.INTER_AREA)
+
             #crop = rotate(crop, 360 - angle)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(crop, imageID, (20, 20), font, 0.4, (0, 0, 0), 2)
@@ -36,7 +31,6 @@ def open_windows():
             cv2.moveWindow(window_name, 80 + (y * 170), 80 + (x * 190))
             cv2.imshow(window_name, crop)
             cv2.setMouseCallback(window_name, call_back, param=[imageID,window_name ])
-            print(2 + time.time()- start)
             cv2.waitKey(5)
     cv2.waitKey(5)
 
@@ -50,25 +44,56 @@ def rotate(img, angle):
 def call_back(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.imshow(param[1], blank_image)
-        if event == cv2.EVENT_FLAG_SHIFTKEY:
-            c.execute('update images set badimage = 1 where imageID = ' + param[0])
+        if flags == cv2.EVENT_FLAG_SHIFTKEY:
+            c.execute('update images set bad_image = 1 where imageID = ' + param[0])
             conn.commit()
         else:
-            c.execute('update images set heads = 1,angle = null where imageID = ' + param[0])
+            c.execute('update images set heads = 0 where imageID = ' + param[0])
             conn.commit()
 
-
     if event == cv2.EVENT_RBUTTONDOWN:
+        print flags
         open_windows()
-
 
 conn = sqlite3.connect('/home/pkrush/2-camera-scripts/coins.db')
 c = conn.cursor()
-c.execute('''Select imageID, angle
+sql = '''
+--these are double headed or double tails:
+Select i2.imageID
+from images i1
+join images i2
+on i1.imageID % 2 = 0
+and i2.imageID % 2 = 1
+and i1.imageID = i2.ImageID - 1
+and not (i1.bad_image = 1 or i2.bad_image = 1)
+and i1.heads = i2.heads
+Order by 1
+'''
+
+sql = '''
+--check tails angle
+Select i2.imageID, i2.angle
+from images i1
+join images i2
+on i1.imageID % 2 = 0
+and i2.imageID % 2 = 1
+and i1.imageID = i2.ImageID - 1
+and not (i1.bad_image = 1 or i2.bad_image = 1)
+and i2.heads = 1
+and i2.angle is not null
+Order by 1'''
+
+
+sql = '''
+--check tails
+Select imageID
 from images
-where bad_image = 0
-and heads = 0
-Order by 1''')
+where heads = 1
+and bad_image = 0
+Order by 1
+'''
+
+c.execute(sql)
 imageIDs = c.fetchall()
 
 thumb_nail_size = 145
@@ -82,20 +107,7 @@ open_windows()
 
 
 while 1==1:
-    cv2.waitKey(5)
     pressed_key = cv2.waitKey(0)
-
-    if pressed_key & 0xFF == ord('a'):
-        sql = 'update images set heads = 1, angle = ' + str(angle) + ' where imageID = ' + imageID
-        print sql
-        c.execute(sql)
-        conn.commit()
-    if pressed_key & 0xFF == ord('b'):
-        c.execute('update images set heads = 1 where imageID = ' + imageID)
-        conn.commit()
-    if pressed_key & 0xFF == ord('t'):
-        c.execute('update images set heads = 0 where imageID = ' + imageID)
-        conn.commit()
     if pressed_key & 0xFF == ord('q'):
         break
 
